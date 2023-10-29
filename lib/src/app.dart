0d00a8e82290aee909a06eda90a7b71e7d0f8d70
Abimelec Garcia <abimelecg2@unitec.edu>
2023-10-29 13:20:12 -0600

    Calculadora en Aplicacion Movil

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tarea2/src/operaciones.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyApp());
+  }
+}
